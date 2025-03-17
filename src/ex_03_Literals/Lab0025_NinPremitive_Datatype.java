942bd45d6d833231c2459c06fe31eff2d40dfa59
HP <rachanams79@gmail.com>
2025-03-18 02:45:52 +0530

    Exercises

---
+++ src/ex_03_Literals/Lab0025_NinPremitive_Datatype.java
@@ -0,0 +1,14 @@
+package ex_03_Literals;
+
+public class Lab0025_NinPremitive_Datatype {
+    public static void main(String[] args) {
+        //Premitive Data Types
+        byte b=10;
+        int age=10;
+
+        //Non Premitive Data types
+        String name="Rachana";
+        int[] arr=new int[10];
+
+    }
+}
