067accbc4e83d7ca67f933379ad1a1cdc442c4b5
HP <rachanams79@gmail.com>
2025-03-18 02:55:31 +0530

    Exercises

---
+++ src/ex_04_Operators/Lab0026_Assignment_Operator.java
@@ -0,0 +1,11 @@
+package ex_04_Operators;
+
+public class Lab0026_Assignment_Operator {
+    public static void main(String[] args) {
+        int age=10;
+        //int -> data type
+        //age -> Identifier
+        //= -> assignment operator
+        //10 -> Literal
+    }
+}
---
+++ src/ex_04_Operators/Lab0027_Arithemetic_Operators.java
@@ -0,0 +1,21 @@
+package ex_04_Operators;
+
+public class Lab0027_Arithemetic_Operators {
+    public static void main(String[] args) {
+        // Arithmetic Operators
+        //        + (Addition)
+        //       - (Subtraction)
+        //       * (Multiplication)
+        //       / (Division)
+        //       % (Modulus) | Modulus - Remainder
+
+        int a=10;
+        int b=5;
+        float c=3f;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a/c);
+    }
+}
